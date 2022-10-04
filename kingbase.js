816b159134f4f0f5a0d1c50e660374f4f26e26d7
MR.TIMA <98197960+TEAM-KING-BOT@users.noreply.github.com>
2022-10-04 11:00:07 +0100

    Update kingbase.js

--- kingbase.js
+++ kingbase.js
@@ -3,4 +3,14 @@ require('../Kingbotmd')
   const _kingbot = require('./startKingbot')
   const kinggit = require('../Kingbotmd')
   
-async function qrGenerate() {
+async function kingbotbase() {
+     await qrgenerate.qr_gen();
+     await kingbase.basetype();
+}
+
+qrGenerate();
+
+_kingbot.Base().catch(() => { _kingbot.Base() });
+
+if (vars.isHEROKU == 'YES') { //will be created soon
+//created by MR.TIMA
